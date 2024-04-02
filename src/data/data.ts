03a3a10f1d7427a911b23508804dce0d1622aeb0
Santiago <santymillan95@gmail.com>
2024-04-01 21:51:48 -0500

    data

---
+++ src/data/data.ts
@@ -0,0 +1,75 @@
+interface usersData {
+	profilepic: string;
+	name: string;
+	image: string;
+	description: string;
+	tags: {
+		tag: string;
+	};
+}
+
+export const data: usersData[] = [
+	{
+		profilepic: 'https://i.pinimg.com/564x/3f/f0/18/3ff0182c281737ce1ac801e29d712d22.jpg',
+		name: 'Santi.gumi',
+		image: 'https://i.pinimg.com/236x/83/c3/94/83c3946cb11a642293136a32f682f027.jpg',
+		description: 'Practicando un poco de ilustración',
+		tags: {
+			tag: 'Ilustration',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/f8/80/89/f88089eb4f760b0032d5b422fcdb775a.jpg',
+		name: 'Pinguin',
+		image: 'https://i.pinimg.com/236x/f1/b7/3c/f1b73c8c51aa1c08451475bcb2384291.jpg',
+		description: 'Retomando la post produccción',
+		tags: {
+			tag: 'Post production',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/03/ff/7f/03ff7fd3437eb3a70c8fcd74db819b0d.jpg',
+		name: 'Monkey09',
+		image: 'https://i.pinimg.com/236x/6b/60/7e/6b607ef51db836d1bbf9e18ba110aba9.jpg',
+		description: 'Me encanta diseñar mundos fantásticos',
+		tags: {
+			tag: 'Fantasy',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/68/74/e8/6874e8f5c9a84bba39e22b25c05b38cc.jpg',
+		name: 'Rattictac',
+		image: 'https://i.pinimg.com/236x/be/cc/4c/becc4cdc1840bbe1c8b70320aad7daba.jpg',
+		description: 'No es mucho, pero he tenido varios avances',
+		tags: {
+			tag: 'Modelado 3d',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/33/73/a2/3373a290bac60b9bc444238c6de50ad6.jpg',
+		name: 'GatofresitaUwu',
+		image: 'https://i.pinimg.com/564x/d0/03/82/d0038298f20e5d2dd2dd10218018de26.jpg',
+		description: 'Nubesita les dice hola :3',
+		tags: {
+			tag: 'Color-teory',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/4f/13/d8/4f13d8d3489a9bd434a0d79da1d07966.jpg',
+		name: 'SkeletonStudio',
+		image: 'https://i.pinimg.com/236x/1e/66/6e/1e666eb2809d59a0069fbc72d8926fe3.jpg',
+		description: 'Un poco de nuestro nuevo rediseño de marca',
+		tags: {
+			tag: 'Branding',
+		},
+	},
+	{
+		profilepic: 'https://i.pinimg.com/564x/a5/81/25/a5812567c1892d5255ddbffd2ec85737.jpg',
+		name: 'Froggy',
+		image: 'https://i.pinimg.com/564x/b9/c6/77/b9c6771500da094ca0751a144d5d0cb2.jpg',
+		description: 'Practica de teoría del color que hice en la universidad',
+		tags: {
+			tag: 'Branding',
+		},
+	},
+];
