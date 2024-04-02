9913c13dbde669a5048e5d3fa8fcf395843b9e9a
Santiago <santymillan95@gmail.com>
2024-04-01 21:56:33 -0500

    Components-buttons-menu

---
+++ src/components/menu/post.ts
@@ -0,0 +1,71 @@
+import styles from './menu.css';
+export enum Attribute2 {
+	'image' = 'image',
+}
+
+class Post extends HTMLElement {
+	image?: string;
+	public index: number= 0
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.onButtonClicked = this.onButtonClicked.bind(this);
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<Attribute2, null> = {
+			image: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	connectedCallback() {
+		this.mount();
+	}
+
+	attributeChangedCallback(propName: Attribute2, oldValue: string | undefined, newValue: string | undefined) {
+		this[propName] = newValue;
+		this.mount();
+	}
+
+	mount() {
+		this.render();
+		this.addListeners();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+            <div class="navBar">
+            <button id="postbutton" type="submit"><img src=${this.image}></button>
+            </div>
+            `;
+		}
+		const cssMenu = this.ownerDocument.createElement('style');
+		cssMenu.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssMenu);
+	}
+
+	addListeners() {
+		this.shadowRoot?.querySelector('#postbutton')?.addEventListener('click', this.onButtonClicked);
+	}
+
+	onButtonClicked() {
+		// const currentValue = this.getAttribute('image');
+		// this.setAttribute('image', '../icons/PostClicked.png');
+		// this.render();
+
+		const currentImage = this.shadowRoot?.querySelector('img')?.getAttribute('src');
+			if (this.index === 0) {
+			this.setAttribute('image', '../icons/PostClicked.png');
+			this.index ++
+			} else {
+			this.setAttribute('image', '../icons/Post.png');
+			this.index --	
+			}
+	}
+}
+
+export default Post;
+customElements.define('post-button', Post);
---
+++ src/components/menu/profile.ts
@@ -0,0 +1,71 @@
+import styles from './menu.css';
+export enum Attribute3 {
+	'image' = 'image',
+}
+
+class Profile extends HTMLElement {
+	image?: string;
+	public index: number= 0
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.onButtonClicked = this.onButtonClicked.bind(this);
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<Attribute3, null> = {
+			image: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	connectedCallback() {
+		this.mount();
+	}
+
+	attributeChangedCallback(propName: Attribute3, oldValue: string | undefined, newValue: string | undefined) {
+		this[propName] = newValue;
+		this.mount();
+	}
+
+	mount() {
+		this.render();
+		this.addListeners();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+            <div class="navBar">
+            <button id="profilebutton" type="submit"><img src=${this.image}></button>
+            </div">
+            `;
+		}
+		const cssMenu = this.ownerDocument.createElement('style');
+		cssMenu.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssMenu);
+	}
+
+	addListeners() {
+		this.shadowRoot?.querySelector('#profilebutton')?.addEventListener('click', this.onButtonClicked);
+	}
+
+	onButtonClicked() {
+		// const currentValue = this.getAttribute('image');
+		// this.setAttribute('image', '../icons/ProfileClicked.png');
+		// this.render();
+
+		const currentImage = this.shadowRoot?.querySelector('img')?.getAttribute('src');
+			if (this.index === 0) {
+			this.setAttribute('image', '../icons/ProfileClicked.png');
+			this.index ++
+			} else {
+			this.setAttribute('image', '../icons/Profile.png');
+			this.index --	
+			}
+	}
+}
+
+export default Profile;
+customElements.define('profile-button', Profile);
