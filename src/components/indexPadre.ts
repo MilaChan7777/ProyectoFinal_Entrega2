4b063ed7d661b00e50d3b501f333b06712f6033e
MilaChan7777 <camila.lerma1@u.icesi.edu.co>
2024-04-01 22:27:52 -0500

    IndexPadre subido

---
+++ src/components/indexPadre.ts
@@ -0,0 +1,4 @@
+export { default as Data } from './userPosts/userPosts';
+export { default as Save } from './buttonPosts/save';
+export { default as Star } from './buttonPosts/star';
+export { default as Comment } from './buttonPosts/comment';
