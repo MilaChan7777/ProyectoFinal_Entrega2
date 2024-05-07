9b18eaa6aa4aec248771f4614d521403ea26ad92
Santiago <santymillan95@gmail.com>
2024-05-07 17:59:40 -0500

    actualizacion

---
+++ src/components/indexPadre.ts
@@ -0,0 +1,4 @@
+export { default as Data } from './userPosts/userPosts';
+export { default as Save } from './buttonPosts/Savebtn/save';
+export { default as Star } from './buttonPosts/Starbtn/star';
+export { default as Comment } from './buttonPosts/Commentbtn/comment';
---
+++ src/components/userPosts/userPosts.ts
@@ -0,0 +1,70 @@
+import styles from './userPosts.css';
+
+export enum Attribute4 {
+	'profilepic' = 'profilepic',
+	'name' = 'name',
+	'image' = 'image',
+	'description' = 'description',
+	'tag' = 'tag',
+}
+
+class usersData extends HTMLElement {
+	profilepic?: string;
+	name?: string;
+	image?: string;
+	description?: string;
+	tag?: string;
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<Attribute4, null> = {
+			profilepic: null,
+			name: null,
+			image: null,
+			description: null,
+			tag: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	attributeChangedCallback(propName: Attribute4, oldValue: string | undefined, newValue: string | undefined) {
+		this[propName] = newValue;
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<section>
+      <div class="userPosts">
+	  <div class="user">
+	  <img src=${this.profilepic}></img>
+      <p class="username">${this.name}</p>
+	  </div>
+		<div class="imgDiv">
+      <img class="imgPost" src=${this.image}></img>
+		</div>
+			<div class="onlyPosts">
+	  <div class="description">
+	  <p class="des">${this.description}</p>
+      <p class="tag">${this.tag}</p>
+			</div>
+	  </div>
+			</section>
+      `;
+		}
+		const cssUserpost = this.ownerDocument.createElement('style');
+		cssUserpost.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssUserpost);
+	}
+}
+
+export default usersData;
+customElements.define('my-usersdata', usersData);
