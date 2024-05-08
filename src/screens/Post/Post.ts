422f5c8920839c997b08eb6a3b3a4c4855597145
Santiago <santymillan95@gmail.com>
2024-05-07 22:48:49 -0500

    Post.ts agg inputPost

--- src/screens/Post/Post.ts
+++ src/screens/Post/Post.ts
@@ -1,3 +1,4 @@
+import { AttributeinputPost } from '../../components/inputPost/inputPost';
 import profileHead, { Attributeposthead } from '../../components/profileHead/profileHead';
 import styles from './Post.css';
 
@@ -15,9 +16,15 @@ class Post extends HTMLElement {
 
 	render() {
 		if(this.shadowRoot){
-		const presentation = document.createElement('profileHead');
-		presentation.setAttribute(Attributeposthead.profilepic, 'https://i.pinimg.com/236x/ab/32/31/ab32318e982048561a4b1f0508b265bb.jpg')
-		presentation.setAttribute(Attributeposthead.name, 'Santi.gumi')
+		const section = document.createElement('section')
+		const profileHead = document.createElement('profileHead');
+		profileHead.setAttribute(Attributeposthead.profilepic, 'https://i.pinimg.com/236x/ab/32/31/ab32318e982048561a4b1f0508b265bb.jpg')
+		profileHead.setAttribute(Attributeposthead.name, 'Santi.gumi')
+		section.appendChild(profileHead)
+
+		const inputPost = document.createElement('inputPost')
+		section.appendChild(inputPost)
+		
 		}
 		const cssUserpost = this.ownerDocument.createElement('style');
 		cssUserpost.innerHTML = styles;
