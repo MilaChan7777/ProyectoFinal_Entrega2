b067cf2c5002f9101ff2029dc1069aad32d8b899
Santiago <santymillan95@gmail.com>
2024-05-09 09:01:12 -0500

    Index actulizado

--- src/components/indexPadre.ts
+++ src/components/indexPadre.ts
@@ -2,3 +2,8 @@ export { default as Data } from './userPosts/userPosts';
 export { default as Save } from './buttonPosts/Savebtn/save';
 export { default as Star } from './buttonPosts/Starbtn/star';
 export { default as Comment } from './buttonPosts/Commentbtn/comment';
+
+export {default as Home } from './menu/Home/Home'
+export { default as Search } from './menu/Search/Search'
+export { default as Post } from './menu/Post/Post'
+export { default as Profile } from './menu/Profile/Profile'
