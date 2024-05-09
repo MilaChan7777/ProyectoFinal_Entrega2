0a02e14ecc2ed71d737252eee9f743243872d551
Santiago <santymillan95@gmail.com>
2024-05-09 09:17:13 -0500

    dispacth Post

--- src/screens/Post/Post.ts
+++ src/screens/Post/Post.ts
@@ -1,17 +1,24 @@
 import { AttributeinputPost } from '../../components/inputPost/inputPost';
 import profileHead, { Attributeposthead } from '../../components/profileHead/profileHead';
 import styles from './Post.css';
-
+import { changeScreen } from '../../store/actions';
+import { addObserver } from '../../store/store';
+import { dispatch } from '../../store/store';
 class Post extends HTMLElement {
 
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+		addObserver(this)
 	}
 
 
 	connectedCallback() {
 		this.render();
+		const Homebutton = this.shadowRoot?.querySelector('#homebutton');
+		Homebutton?.addEventListener('click', ()=>{
+		dispatch(changeScreen('DASHBOARD'))
+		})
 	}
 
 	render() {
