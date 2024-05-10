83b61c74fd73aeeb34b8e71325528b0a018b575e
Santiago <santymillan95@gmail.com>
2024-05-10 14:56:47 -0500

    new component

---
+++ src/components/profileHead2/profileHead2.ts
@@ -0,0 +1,53 @@
+import styles from './profileHead2.css';
+
+export enum Attributeposthead2 {
+	'profilepic' = 'profilepic',
+	'name' = 'name',
+	'followers' = 'followers'
+}
+
+class Profilehead extends HTMLElement {
+	profilepic?: string;
+	name?: string;
+	followers?: string
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		const attrs: Record<Attributeposthead2, null> = {
+			profilepic: null,
+			name: null,
+			followers: null
+		};
+		return Object.keys(attrs);
+	}
+
+	attributeChangedCallback(propName: Attributeposthead2, oldValue: string | undefined, newValue: string | undefined) {
+		this[propName] = newValue;
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+	        <img class="userimg" src=${this.profilepic}></img>
+			<div class="info">
+			<p class="username">${this.name}</p>
+			<p class="followers">${this.followers} seguidores</p>
+			</div>
+            `;
+		    }
+		const cssUserpost = this.ownerDocument.createElement('style');
+		cssUserpost.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssUserpost);
+	}
+}
+
+export default Profilehead;
+customElements.define('app-profilehead2', Profilehead);
