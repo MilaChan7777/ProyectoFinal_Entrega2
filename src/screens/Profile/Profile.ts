884f51049a6ad9149b106a4c3fc54bdcd946448c
Santiago <santymillan95@gmail.com>
2024-05-10 14:09:59 -0500

    new element

--- src/screens/Profile/Profile.ts
+++ src/screens/Profile/Profile.ts
@@ -1,7 +1,7 @@
 
 import profileHead, { Attributeposthead } from '../../components/profileHead/profileHead';
 import Profileinter, { AttributeprofileInter } from '../../components/profileInter/profileInter';
-import styles from './Post.css';
+import styles from './Profile.css';
 
 class Profile extends HTMLElement {
 
@@ -18,15 +18,16 @@ class Profile extends HTMLElement {
 	render() {
 		if(this.shadowRoot){
 		const section = document.createElement('section')
-		const profileHead = document.createElement('profileHead');
+		const profileHead = document.createElement('app-profilehead');
 		profileHead.setAttribute(Attributeposthead.profilepic, 'https://i.pinimg.com/236x/ab/32/31/ab32318e982048561a4b1f0508b265bb.jpg')
 		profileHead.setAttribute(Attributeposthead.name, 'Santi.gumi')
         profileHead.setAttribute(Attributeposthead.followers, '41')
 		section.appendChild(profileHead)
 
-        const profileInput = document.createElement('profileInter');
+        const profileInput = document.createElement('profile-inter');
         profileInput.setAttribute(AttributeprofileInter.description, 'Ilustrador colombiano de 19 años, apasionado por el arte fantástico y estudiante de diseño de medios interactivos.')
-
+		section.appendChild(profileInput)
+		this.shadowRoot.appendChild(section)
 		}
 		const cssUserpost = this.ownerDocument.createElement('style');
 		cssUserpost.innerHTML = styles;
@@ -35,4 +36,4 @@ class Profile extends HTMLElement {
 }
 
 export default Profile;
-customElements.define('profile', Profile);
+customElements.define('app-profile', Profile);
