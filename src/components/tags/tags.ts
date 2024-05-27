685a3a58b344ab266280af8b71c45b9f7a5539d7
Santiago <santymillan85@gmail.com>
2024-05-27 18:42:40 -0500

    nuevos componentes

---
+++ src/components/tags/tags.ts
@@ -0,0 +1,28 @@
+import styles from './tags.css';
+
+class Tags extends HTMLElement {
+	description?: string;
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+            
+            `;
+		    }
+		const cssUserpost = this.ownerDocument.createElement('style');
+		cssUserpost.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssUserpost);
+	}
+}
+
+export default Tags;
+customElements.define('app-tags', Tags);
