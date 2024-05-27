7d84ba55fb44679a94ca1f519ed76e9c138bd739
Santiago <santymillan85@gmail.com>
2024-05-27 14:51:52 -0500

    search delete

--- src/components/indexPadre.ts
+++ src/components/indexPadre.ts
@@ -4,7 +4,6 @@ export { default as Star } from './buttonPosts/Starbtn/star';
 export { default as Comment } from './buttonPosts/Commentbtn/comment';
 
 export {default as Home } from './menu/Home/Home'
-export { default as Search } from './menu/Search/Search'
 export { default as Post } from './menu/Post/Post'
 export { default as Profilebtn } from './menu/Profile/Profile'
 
