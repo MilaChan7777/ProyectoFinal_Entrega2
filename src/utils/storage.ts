ab285b05f08a44c5ce75a67426a68c31df68dc0e
Santiago <santymillan85@gmail.com>
2024-06-02 10:43:40 -0500

    storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,18 @@
+export enum PersistenceKey {
+    'POST' = 'POST'
+}
+
+const get = (key: PersistenceKey, defaultValue: unknown) => {
+    const value = localStorage.getItem(key) || sessionStorage.getItem(key)
+    return value ? JSON.parse(value) : defaultValue;
+}
+const set = ({key, value, session = false}: {key: PersistenceKey, value: unknown, session: boolean}) => {
+    const storage = session ? sessionStorage : localStorage
+    const parsed = JSON.stringify(value);
+    storage.setItem(key, parsed)
+} 
+
+export default {
+    get,
+    set
+}
