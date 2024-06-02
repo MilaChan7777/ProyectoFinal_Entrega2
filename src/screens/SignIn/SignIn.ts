d9297fbfa3828204e26636d4ebad626b7bdb55d6
Santiago <santymillan85@gmail.com>
2024-06-02 15:24:26 -0500

    add create user

--- src/screens/SignIn/SignIn.ts
+++ src/screens/SignIn/SignIn.ts
@@ -2,6 +2,13 @@ import { changeScreen } from '../../store/actions';
 import { addObserver } from '../../store/store';
 import { dispatch } from '../../store/store';
 import styles from './SignIn.css';
+import { createUser  } from '../../utils/firebase';
+import { AttributeinputInfo } from '../../components/inputsInfo/inputsInfo';
+
+const FormData = {
+	email: '',
+	password: ''
+}
 
 class SignIn extends HTMLElement {
 	constructor() {
@@ -32,28 +39,68 @@ class SignIn extends HTMLElement {
 			css.innerHTML = styles;
 			this.shadowRoot?.appendChild(css);
 
-			this.shadowRoot.innerHTML += `
-                <main style="display: flex; flex-direction: row; align-items: center; justify-content: center;">
-                    <div class="form-container">
-                        <form>
-												<h1 class="vanguardTitle">Vanguard</h1>
-												<p class="slogan">Create and connect</p>
-                            <h4>Username</h4>
-                            <input type="text" placeholder="" />
-                            <h4>Birthday</h4>
-                            <input type="date" placeholder=""/>
-                            <h4>E-mail</h4>
-                            <input type="email" placeholder="" />
-                            <h4>Password</h4>
-                            <input type="password" placeholder="" />
-                        </form>
-												<button id="signinButton">Sign in</button>
+			const main = document.createElement('main')
+			this.shadowRoot.appendChild(main)
 
-                        <button id="loginButton">Already have an account?</button>
-                    </div>
-                </main>
-            `;
+			const section = document.createElement('section')
+			section.className = "form-container"
+			main.appendChild(section)
+
+			const vanguardTitle = document.createElement('h1')
+			vanguardTitle.className = 'vanguardTitle'
+			vanguardTitle.innerText = 'Vanguard'
+			section.appendChild(vanguardTitle)
+
+			const slogan = document.createElement('p')
+			slogan.className = 'slogan'
+			slogan.innerText = 'Create and connect'
+			section.appendChild(slogan)
+
+			const form = document.createElement('form')
+			section.appendChild(form)
+
+			const email = document.createElement ('input-info')
+			email.setAttribute(AttributeinputInfo.titulo, 'Email')
+			email.setAttribute(AttributeinputInfo.placeholder, 'Email')
+			email.setAttribute(AttributeinputInfo.type, 'email')
+			email.addEventListener('change', this.addEmail)
+			form.appendChild(email)
+
+			const birthday = document.createElement ('input-info')
+			birthday.setAttribute(AttributeinputInfo.titulo, 'Birthday')
+			birthday.setAttribute(AttributeinputInfo.placeholder, '')
+			birthday.setAttribute(AttributeinputInfo.type, 'date')
+			form.appendChild(birthday)
+
+			const password = document.createElement ('input-info')
+			password.setAttribute(AttributeinputInfo.titulo, "Password")
+			password.setAttribute(AttributeinputInfo.placeholder, 'Password')
+			password.setAttribute(AttributeinputInfo.type, 'password')
+			password.addEventListener('change', this.addPassword)
+			form.appendChild(password)
+
+			const signInButton = document.createElement('button');
+			signInButton.id = 'signUpButton'
+			signInButton.innerText = 'Sign in'
+			form.appendChild(signInButton)
+			
+			const loginButton = document.createElement('button');
+			loginButton.id = 'logInButton'
+			loginButton.innerText = 'Already have an account?'
+			form.appendChild(loginButton)
 		}
+	}
+
+	addEmail(e: any){
+	FormData.email = e?.target?.Value
+	}
+
+	addPassword(e: any){
+	FormData.password = e?.target?.Value
+	}
+
+	submitForm(){
+	createUser(FormData.email, FormData.password)
 	}
 }
 
