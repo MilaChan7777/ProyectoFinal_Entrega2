0e986b27df0c001641914b86b522ee95417dd840
Santiago <santymillan85@gmail.com>
2024-06-03 16:56:15 -0500

    getPostAction added

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,15 @@
+import { getPosts } from "../utils/firebase"
 export const changeScreen = (screen: any) => {
     return {
         action: 'changeScreen',
         payload: screen,
+    }
+}
+
+export const getPostsAction = async () => {
+    const posts  = await getPosts()
+    return {
+        action: 'GETPOST',
+        payload: posts
     }
 }
