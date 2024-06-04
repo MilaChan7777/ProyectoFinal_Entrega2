f281f754e67a6db746ff242119be426c31ae7104
Santiago <santymillan85@gmail.com>
2024-06-03 19:07:12 -0500

    add onAuthStateChanged

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,29 +1,39 @@
 import { reducer } from './reducers';
 import Storage from '../utils/storage';
 import { PersistenceKey } from '../utils/storage';
+import { onAuthStateChanged } from 'firebase/auth';
+import { Screens } from '../types/navigation';
+import { auth } from '../utils/firebase';
+import { changeScreen, setUserCredentials } from './actions';
 
-export let emptyState = {
-	screen: 'SIGNIN',
+
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		user.uid !== null ? dispatch(setUserCredentials(user.uid)) : '';
+		dispatch(changeScreen(Screens.DASHBOARD));
+	} else {
+		dispatch(changeScreen(Screens.LOGIN));
+	}
+});
+
+const emptyState = {
+	screen: Screens.LOGIN,
 	post: [],
+	user: {},
 };
-
-export let appState = Storage.get({key:  PersistenceKey.STATE, defaultValue: emptyState})
+export let appState = emptyState
+	// export let appState = Storage.get({key:  PersistenceKey.STATE, defaultValue: emptyState})
 
 let observers: any = [];
 
-const persisStore = (state: any) => {
-	Storage.set({key: PersistenceKey.STATE, value: state, session: false})
-}
+
 const notifyObservers = () => observers.forEach((o: any) => o.render());
 
 export const dispatch = (action: any) => {
 	const clone = JSON.parse(JSON.stringify(appState));
 	const newState = reducer(action, clone);
 	appState = newState;
-
-	persisStore(newState)
-	notifyObservers()
-	observers.forEach((o: any) => o.render());
+	notifyObservers();
 };
 
 export const addObserver = (ref: any) => {
